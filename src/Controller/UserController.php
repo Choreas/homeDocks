0394accc8299712f63f97b7352c0cae244e33414
Choreas <niklas.kosanke@web.de>
2021-02-18 12:14:02 +0100

    general styling and logout button

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,11 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController {
-  /**
+    /**
      * @Route("/", name="landing")
      */
     public function landing(): Response
     {
-        return $this->render('landing.html.twig');
+        return $this->render('landing.html.twig', ['user' => $this->getUser()]);
     }
 }
